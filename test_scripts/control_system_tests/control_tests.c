facc63df56318c1c342190612bbaaaa6cbf53e4b
Harsh <chittoraharsh98@gmail.com>
2021-11-08 22:10:01 +0530

    making int controller instead of uint

--- test_scripts/control_system_tests/control_tests.c
+++ test_scripts/control_system_tests/control_tests.c
@@ -11,26 +11,23 @@ int main(){
     DDRB = 0xFE;
     TCCR1A = 1 << COM1B1 | 1<< WGM10 | 1 << WGM11;
     TCCR1B = 1 << WGM12 | 1 << CS10;
-    uint16_t d = 0, duty = 0, error = 0;
-    uint16_t ref = 0x2CC;
-    uint16_t div = 4;
+    //uint16_t d = 0, duty = 0, error = 0;
+    int ref = 0x200;
+    int div = 4;
+    int d = 0, duty = 0, error = 0;
     while(1){
-        _delay_ms(2);
+        
+        _delay_ms(1);
         d = ADCL | ADCH << 8;
-        if(ref > d){
-            error = (ref - d) / div;
-            duty += error;
-        }
-        else{
-            error = (d - ref) / div;
-            duty -= error;
-        }
-        if(duty & 0x1000){
+        error = (ref - d)/div;
+        duty += error;
+        if(duty < 0){
             duty = 0;
         }
         if(duty > 0x3FF){
             duty = 0x3FF;
         }
+        
         
         OCR1B = duty;
     }
